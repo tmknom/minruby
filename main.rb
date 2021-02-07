37ed1aa68159b33ab1a1a44d360bc3c89fda7354
tmknom <tmk.nom@gmail.com>
2021-02-07 17:02:49 +0900

    最大の葉に対応

--- main.rb
+++ main.rb
@@ -35,12 +35,28 @@ def arithmetic(op, left, right)
   end
 end
 
+def max_leaf(tree)
+  if tree[0] == "lit"
+    return tree[1]
+  end
+
+  left = max_leaf(tree[1])
+  right = max_leaf(tree[2])
+
+  if left > right
+    left
+  else
+    right
+  end
+end
+
 def main
   # str = gets
-  str = "5 * (2 + 3)"
+  str = "5 * 12 + 6 / 2"
   tree = minruby_parse(str)
   p(tree)
   p(evaluate(tree))
+  p(max_leaf(tree))
 end
 
 main
