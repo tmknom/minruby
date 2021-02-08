5a26327bdbf2cb2840c65e3bb9e79e07e0f0413a
tmknom <tmk.nom@gmail.com>
2021-02-08 18:32:07 +0900

    ブートストラップしたうえでFizzBuzzを実行

--- test.rb
+++ test.rb
@@ -1,5 +1,17 @@
-hsh = { "a" => 10, "b" => 20, "c" => 30 }
-hsh["c"] = 42
-p(hsh["a"])
-p(hsh["b"])
-p(hsh["c"])
+i = 1
+while i <= 100
+  if i % 3 == 0
+    if i % 5 == 0
+      p("FizzBuzz")
+    else
+      p("Fizz")
+    end
+  else
+    if i % 5 == 0
+      p("Buzz")
+    else
+      p(i)
+    end
+  end
+  i = i + 1
+end
